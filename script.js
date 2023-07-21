97368a642fd7703a2edcad4f13d9f1bac76971ef
Vedant Raut <115027415+vraut544@users.noreply.github.com>
2023-07-21 19:41:55 +0530

    added all the other files

---
+++ script.js
@@ -0,0 +1,44 @@
+// Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// Scroll section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = set.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if(top >= offset && top < offset + height){
+            //Active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            });
+
+            // active sections for animations on scroll
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+
+    });
+    // Sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY  > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
